a09ee6ab5de99a5ad15d2b0bd1823c6a0cdfe2c1
Lance Eugene <lanceeugene.magtaka.cics@ust.edu.ph>
2023-03-18 17:26:38 +0800

    override the displayInfo() method

--- OOP.php
+++ OOP.php
@@ -44,13 +44,14 @@
 			$this->boolVar = $bool;
 		}
 
-		// method to display info
-		public function displayInfo() {
-			echo "String: " . $this->strVar . "<br>";
-			echo "Integer: " . $this->intVar . "<br>";
-			echo "Boolean: " . ($this->boolVar ? 'true' : 'false') . "<br>";
-		}
-	}
+		
+    // method to display info
+    public function displayInfo() {
+        echo "String: " . $this->strVar . "<br>";
+        echo "Integer: " . $this->intVar . "<br>";
+        echo "Boolean: " . ($this->boolVar ? 'true' : 'false') . "<br>";
+    }
+}
 
 	// child class
 	class ChildClass extends ParentClass {
@@ -67,87 +68,25 @@
 			return $this->newVar;
 		}
 
-		// method to display info, including newVar
-		public function printInfo() {
-			parent::displayInfo();
-			echo "New: " . $this->newVar . "<br>";
-		}
-	}
-
-	// instantiate objects
-	$obj1 = new ParentClass("hello", 123, true);
-	$obj2 = new ChildClass("world", 456, false, "new");
-
-	// display object info
-	echo "<h3>Parent Object:</h3>";
-	$obj1->displayInfo();
-	echo "<h3>Child Object:</h3>";
-	$obj2->printInfo();
-
-	// grandchild class
-class GrandChildClass extends ChildClass {
-    private $extraVar;
-
-    // constructor with additional parameter
-    public function __construct($str, $int, $bool, $new, $extra) {
-        parent::__construct($str, $int, $bool, $new);
-        $this->extraVar = $extra;
-    }
-
-    // getter for extraVar
-    public function getExtraVar() {
-        return $this->extraVar;
-    }
-
-    // method to display info, including extraVar
-    public function printExtraInfo() {
-        parent::printInfo();
-        echo "Extra: " . $this->extraVar . "<br>";
-    }
-}
-
-// instantiate objects
-$obj3 = new GrandChildClass("hello", 123, true, "new", "extra");
-
-// display object info
-echo "<h3>Grandchild Object:</h3>";
-$obj3->printExtraInfo();
-
-// second child class
-class SecondChildClass extends ParentClass {
-    private $secondVar;
-
-    // constructor with additional parameter
-    public function __construct($str, $int, $bool, $second) {
-        parent::__construct($str, $int, $bool);
-        $this->secondVar = $second;
-    }
-
-    // getter for secondVar
-    public function getSecondVar() {
-        return $this->secondVar;
-    }
-
-    // method to display info, including secondVar
-    public function printSecondInfo() {
+		
+    // override parent's displayInfo method
+    public function displayInfo() {
+        echo "Child Class Display Info: <br>";
         parent::displayInfo();
-        echo "Second: " . $this->secondVar . "<br>";
+        echo "New: " . $this->newVar . "<br>";
     }
 }
 
 // instantiate objects
-$obj4 = new SecondChildClass("hello", 123, true, "second");
+$obj1 = new ParentClass("hello", 123, true);
+$obj2 = new ChildClass("world", 456, false, "new");
 
 // display object info
-echo "<h3>Second Child Object:</h3>";
-$obj4->printSecondInfo();
-
 echo "<h3>Parent Object:</h3>";
 $obj1->displayInfo();
+echo "<h3>Child Object:</h3>";
+$obj2->displayInfo();
 
-
-
-	
 	?>
 
 </body>
