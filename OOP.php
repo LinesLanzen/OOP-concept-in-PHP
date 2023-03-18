d23d14dec86798f88a21fd6e147cf4cccdb986a1
Lance Eugene <lanceeugene.magtaka.cics@ust.edu.ph>
2023-03-18 17:42:29 +0800

    Overload the printInfo() method

--- OOP.php
+++ OOP.php
@@ -7,44 +7,43 @@
 
 	<?php
 
-	// parent class
-	class ParentClass {
-		private $strVar;
-		private $intVar;
-		private $boolVar;
+// parent class
+class ParentClass {
+    private $strVar;
+    private $intVar;
+    private $boolVar;
 
-		// constructor
-		public function __construct($str, $int, $bool) {
-			$this->strVar = $str;
-			$this->intVar = $int;
-			$this->boolVar = $bool;
-		}
+    // constructor
+    public function __construct($str, $int, $bool) {
+        $this->strVar = $str;
+        $this->intVar = $int;
+        $this->boolVar = $bool;
+    }
 
-		// getter and setter for strVar
-		public function getStrVar() {
-			return $this->strVar;
-		}
-		public function setStrVar($str) {
-			$this->strVar = $str;
-		}
+    // getter and setter for strVar
+    public function getStrVar() {
+        return $this->strVar;
+    }
+    public function setStrVar($str) {
+        $this->strVar = $str;
+    }
 
-		// getter and setter for intVar
-		public function getIntVar() {
-			return $this->intVar;
-		}
-		public function setIntVar($int) {
-			$this->intVar = $int;
-		}
+    // getter and setter for intVar
+    public function getIntVar() {
+        return $this->intVar;
+    }
+    public function setIntVar($int) {
+        $this->intVar = $int;
+    }
 
-		// getter and setter for boolVar
-		public function getBoolVar() {
-			return $this->boolVar;
-		}
-		public function setBoolVar($bool) {
-			$this->boolVar = $bool;
-		}
+    // getter and setter for boolVar
+    public function getBoolVar() {
+        return $this->boolVar;
+    }
+    public function setBoolVar($bool) {
+        $this->boolVar = $bool;
+    }
 
-		
     // method to display info
     public function displayInfo() {
         echo "String: " . $this->strVar . "<br>";
@@ -53,27 +52,31 @@
     }
 }
 
-	// child class
-	class ChildClass extends ParentClass {
-		private $newVar;
+// child class
+class ChildClass extends ParentClass {
+    private $newVar;
 
-		// constructor with additional parameter
-		public function __construct($str, $int, $bool, $new) {
-			parent::__construct($str, $int, $bool);
-			$this->newVar = $new;
-		}
+    // constructor with additional parameter
+    public function __construct($str, $int, $bool, $new) {
+        parent::__construct($str, $int, $bool);
+        $this->newVar = $new;
+    }
 
-		// getter for newVar
-		public function getNewVar() {
-			return $this->newVar;
-		}
+    // getter for newVar
+    public function getNewVar() {
+        return $this->newVar;
+    }
 
-		
-    // override parent's displayInfo method
+    // override the displayInfo() method to add newVar
     public function displayInfo() {
-        echo "Child Class Display Info: <br>";
         parent::displayInfo();
         echo "New: " . $this->newVar . "<br>";
+    }
+
+    // overload the printInfo() method to accept a parameter
+    public function printInfo($param) {
+        echo "Parameter: " . $param . "<br>";
+        $this->displayInfo();
     }
 }
 
@@ -85,7 +88,8 @@ $obj2 = new ChildClass("world", 456, false, "new");
 echo "<h3>Parent Object:</h3>";
 $obj1->displayInfo();
 echo "<h3>Child Object:</h3>";
-$obj2->displayInfo();
+$obj2->printInfo("hello");
+
 
 	?>
 
